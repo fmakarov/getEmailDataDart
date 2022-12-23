9bb5096977c8494e4bae3dc62b9105b38ae905ef
Marsel <digr15377@gmail.com>
2022-12-23 15:50:09 +0300

    вывод сообщений

--- bin/dart.dart
+++ bin/dart.dart
@@ -1,53 +1,57 @@
-import 'dart:convert'; 
-import 'package:http/http.dart'; 
- 
-var message_id = []; 
- 
-void main() async { 
-  Object email = await fetchEmail(); 
-  fetchData(email); 
-} 
- 
-var url = 
-    Uri.https("www.1secmail.com", "/api/v1/", {"action": "genRandomMailbox"}); 
- 
-Future<Object> fetchEmail() async { 
-  try { 
-    var response = await get(url); 
-    var email = json.decode(response.body)[0].toString(); 
-    return email; 
-  } catch (err) { 
-    return err; 
-  } 
-} 
- 
-fetchData(email) async { 
-  var login = email.split("@")[0]; 
-  var domain = email.split("@")[1]; 
- 
-  print(email); 
- 
-  var url2 = Uri.https("www.1secmail.com", "/api/v1/", 
-      {"action": "getMessages", "login": login, "domain": domain}); 
-   
-   
-   
-  while (true) { 
-    try { 
-      var resp = await get(url2); 
-      var respList = json.decode(resp.body); 
-      print(respList); 
-    } catch (err) { 
-      return err; 
-    } 
-    await Future.delayed(Duration(seconds: 20)); 
-  } 
-  try { 
-    m = messages[id]; 
-    if m not in message_id: 
-      info = get_message(email, m) 
-      message_id.add(m) #add переносит в конец списка
-      print () 
-         
-  }  
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  Object email = await fetchEmail();
+  fetchData(email);
+  while (true) {
+    await fetchData(email);
+    await Future.delayed(Duration(seconds: 20));
+  }
+}
+
+var url =
+    Uri.https("www.1secmail.com", "/api/v1/", {"action": "genRandomMailbox"});
+
+Future<Object> fetchEmail() async {
+  try {
+    var response = await http.get(url);
+    var email = json.decode(response.body)[0].toString();
+    return email;
+  } catch (err) {
+    return err;
+  }
+}
+
+fetchData(email) async {
+  var login = email.split("@")[0];
+  var domain = email.split("@")[1];
+
+  print(email);
+
+  var url2 = Uri.http("www.1secmail.com", "/api/v1/",
+      {"action": "getMessages", "login": login, "domain": domain});
+//Future<void> fetchData();
+
+  while (true) {
+    try {
+      var resp = await http.get(url2);
+      var respList = json.decode(resp.body);
+      //print(respList);
+
+      var m_id = respList[0]["id"];
+      print(m_id);
+      var url3 = Uri.http("www.1secmail.com", "/api/v1/",
+          {"action": "getMessages", "login": login, "domain": domain});
+      var url33 = http.get(url3);
+      print(url33);
+      //print(url3);
+      //var resp1 = await http.get(url3);
+      //var respList1 = json.decode(resp1.body);
+      print(url3);
+    } catch (err) {
+      return err;
+    }
+    await Future.delayed(Duration(seconds: 20));
+  }
 }
